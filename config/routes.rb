8f5cce1cdf7f16ca95f30a7f5c782af73a11ec5c
Josh Van Wyk <joshvanwyk@ymail.com>
2017-08-15 22:06:42 +0000

    Guest controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'guest/index'
+  
+  root 'guest#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
